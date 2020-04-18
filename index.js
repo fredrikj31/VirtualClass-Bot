722d7e9e33585ae440214b0de8c53a5bf37d8900
fredrikj31 <fredrik1206@gmail.com>
2020-04-18 18:17:21 +0200

    Added Present command

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ client.on("ready", () => {
 
 // Hands reaction
 client.on("messageReactionAdd", async (reaction, user) => {
-
 	if (user.bot) return;
 
 	// When we receive a reaction we check if the reaction is partial or not
@@ -43,15 +42,17 @@ client.on("messageReactionAdd", async (reaction, user) => {
 	console.log("-----------------");
 	console.log(user)*/
 
-	console.log(
+	/*console.log(
 		`${user.username} reacted on ${reaction.message.author.username}'s message with ${reaction.emoji}`
-	);
+	);*/
 
 	if (reaction.message.author.username === client.user.username) {
 		if (handCooldown.has(user.id)) {
-			user.send("Do not spam this function. You can use this every second.");
+			user.send(
+				"Do not spam this function. You can use this every second."
+			);
 		} else {
-			if (reaction.emoji.name === "✋" || reaction.emoji.name === "👍") {
+			if (reaction.emoji.name === "✋") {
 				//Function
 				console.log("You reacted with thumbs up.");
 				//? Function that adds user to role and edits the username
@@ -72,8 +73,8 @@ client.on("messageReactionAdd", async (reaction, user) => {
 				//Adds the cooldown
 				handCooldown.add(user.id);
 				setTimeout(() => {
-				  // Removes the user from the set after a minute
-				  handCooldown.delete(user.id);
+					// Removes the user from the set after a minute
+					handCooldown.delete(user.id);
 				}, 1000);
 			}
 		}
@@ -81,7 +82,6 @@ client.on("messageReactionAdd", async (reaction, user) => {
 });
 
 client.on("messageReactionRemove", async (reaction, user) => {
-
 	if (user.bot) return;
 
 	// When we receive a reaction we check if the reaction is partial or not
@@ -99,25 +99,27 @@ client.on("messageReactionRemove", async (reaction, user) => {
 		}
 	}
 
-	console.log(
+	/*console.log(
 		`${user.username} removed his/her reaction on ${reaction.message.author.username}'s message with ${reaction.emoji}`
-	);
+	);*/
 
 	if (reaction.message.author.username === client.user.username) {
 		if (handCooldown.has(user.id)) {
-			user.send("Do not spam this function. You can use this every second.");
+			user.send(
+				"Do not spam this function. You can use this every second."
+			);
 		} else {
-			if (reaction.emoji.name === "✋" || reaction.emoji.name === "👍") {
+			if (reaction.emoji.name === "✋") {
 				console.log("You reacted with thumbs up.");
 				//? Function that adds user to role and edits the username
 				let handRole = reaction.message.guild.roles.cache.find(
 					(role) => role.name === "Hand"
 				);
 				let userGuild = reaction.message.guild.members.fetch(user);
-		
+
 				//Added to hands role
 				(await userGuild).roles.remove(handRole.id);
-		
+
 				//Change their nickname
 				console.log((await userGuild).displayName);
 				let stripedUsername = (await userGuild).displayName
@@ -128,8 +130,8 @@ client.on("messageReactionRemove", async (reaction, user) => {
 				//Adds the cooldown
 				handCooldown.add(user.id);
 				setTimeout(() => {
-				  // Removes the user from the set after a minute
-				  handCooldown.delete(user.id);
+					// Removes the user from the set after a minute
+					handCooldown.delete(user.id);
 				}, 1000);
 			}
 		}
@@ -151,12 +153,6 @@ client.on("message", async (message) => {
 				msg.createdAt - message.createdAt
 			)}ms`
 		);
-	}
-
-	if (command === "say") {
-		const sayMessage = args.join(" ");
-		message.delete().catch((O_o) => {});
-		message.channel.send(sayMessage);
 	}
 
 	if (command === "moveall") {
@@ -186,33 +182,53 @@ client.on("message", async (message) => {
 	if (command === "setuphand") {
 		message.delete();
 		const exampleEmbed = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle('✋ Raise/Lower Hand')
-			.setDescription("You need to react to this message, but do not spam it.")
+			.setColor("#0099ff")
+			.setTitle("✋ Raise/Lower Hand")
+			.setDescription(
+				"You need to react to this message, but do not spam it."
+			)
 			.addFields(
-				{ name: 'To raise your hand.', value: 'React with this emoji: ✋'},
-				{ name: 'To lower your hand.', value: 'Remove your reaction.'},
+				{
+					name: "To raise your hand.",
+					value: "React with this emoji: ✋",
+				},
+				{ name: "To lower your hand.", value: "Remove your reaction." }
 			)
 			.setTimestamp()
-			.setFooter('Setup Timestamp');
+			.setFooter("Setup Timestamp");
 
 		var embedMSG = message.channel.send(exampleEmbed);
 
-		(await embedMSG).react("✋")
+		(await embedMSG).react("✋");
 	}
 
 	if (command == "purge") {
-		const amount = args.join(' '); // Amount of messages which should be deleted
+		const amount = args[0]; // Amount of messages which should be deleted
 
-		if (!amount) return message.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
-		if (isNaN(amount)) return message.reply('The amount parameter isn`t a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
+		if (!amount)
+			return message.reply(
+				"You haven't given an amount of messages which should be deleted!"
+			); // Checks if the `amount` parameter is given
+		if (isNaN(amount))
+			return message.reply("The amount parameter isn`t a number!"); // Checks if the `amount` parameter is a number. If not, the command throws an error
 
-		if (amount > 50) return message.reply('You can`t delete more than 50 messages at once!'); // Checks if the `amount` integer is bigger than 100
-		if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
+		if (amount > 50)
+			return message.reply(
+				"You can`t delete more than 50 messages at once!"
+			); // Checks if the `amount` integer is bigger than 100
+		if (amount < 1)
+			return message.reply("You have to delete at least 1 message!"); // Checks if the `amount` integer is smaller than 1
 
-		await message.channel.messages.fetch({ limit: amount+1 }).then(messages => { // Fetches the messages
-			message.channel.bulkDelete(messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-		)});
+		await message.channel.messages
+			.fetch({ limit: amount })
+			.then((messages) => {
+				// Fetches the messages
+				message.channel.bulkDelete(
+					messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
+				);
+			});
+
+		console.log(`Deleted ${amount} messages`);
 	}
 
 	if (command === "servercache") {
@@ -221,6 +237,94 @@ client.on("message", async (message) => {
 
 	if (command === "test") {
 		console.log(client.user.username);
+	}
+
+	if (command === "absence") {
+		//Command: /absence <time before result>
+
+		userTyped = message.author;
+
+		if (!args[0])
+			return message.reply(
+				":x: You need to define a custom time in minutes. !absence <time in minutes>"
+			);
+		if (isNaN(args[0]))
+			return message.reply(
+				":x: You did not specify a number. !absence <time in minutes>"
+			);
+
+		var timeLeft = args[0] * 60 * 1000;
+
+		message.delete();
+
+		const exampleEmbed = new MessageEmbed()
+			.setColor("#0099ff")
+			.setTitle("✅ Present Check")
+			.setDescription(
+				"If you are present this lesson, react to this message."
+			)
+			.addFields({
+				name: "React to be absence.",
+				value: "React with this emoji: ✅",
+			})
+			.setTimestamp()
+			.setFooter("Absence Date: ");
+
+		var absenceMessage = message.channel.send(exampleEmbed);
+
+		var timeCreated = (await absenceMessage).createdAt;
+
+		(await absenceMessage).react("✅");
+
+		const filter = (reaction, user) => reaction.emoji.name === "✅";
+
+		const collector = (await absenceMessage).createReactionCollector(
+			filter,
+			{ time: 10000 }
+		);
+		//collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+		collector.on("end", (collected) => {
+			let reaction = collected.first();
+			//console.log(reaction);
+			//console.log("-----------------------");
+			//console.log(reaction.users.cache);
+			var i = 1;
+			var finalList = [];
+			var creatingList = new Promise((resolve, reject) => {
+				reaction.users.cache.forEach((element) => {
+					if (i == 1) {
+						i += 1;
+						return;
+					}
+					i += 1;
+					//console.log(element)
+					var serverUsername = message.guild.members.fetch(
+						element.id
+					);
+					serverUsername.then((user) => {
+						//console.log(user.nickname)
+						finalList.push(user.nickname);
+					});
+				});
+				resolve();
+			});
+			creatingList.then(() => {
+				//console.log(finalList);
+
+				topLine = "------------------------------------------------------------- **Present Status** -------------------------------------------------------------\n"
+				bottomLine = "\n---------------------------------------------------------------------------------------------------------------------------------------------"
+
+				outputMessage = topLine + "The present status you created at: " + "`" + timeCreated + "` is finish, and the results are here. \n\n **These students were present:**\n" + finalList.map((item, i) => `${i + 1}. ${item}`).join("\r\n") + bottomLine
+
+				userTyped.send(outputMessage);
+			});
+		});
+	}
+
+	if (command === "help") {
+	}
+
+	if (command === "commands") {
 	}
 });
 
