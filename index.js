a6707a97ba09d2a9c8ac97932b5df921fddf3f5d
fredrikj31 <fredrik1206@gmail.com>
2020-04-18 18:29:04 +0200

    Added permissions

--- index.js
+++ index.js
@@ -146,179 +146,199 @@ client.on("message", async (message) => {
 	const command = args.shift().toLowerCase();
 
 	if (command === "ping") {
-		const msg = await message.channel.send(`🏓 Pinging....`);
+		if (message.member.roles.some(role => role.name === 'Teacher')) {
+			const msg = await message.channel.send(`🏓 Pinging....`);
 
-		msg.edit(
-			`🏓 Pong! Latency is ${Math.floor(
-				msg.createdAt - message.createdAt
-			)}ms`
-		);
+			msg.edit(
+				`🏓 Pong! Latency is ${Math.floor(
+					msg.createdAt - message.createdAt
+				)}ms`
+			);
+		} else {
+			message.reply(":x: You don't have permission to use this command.");
+		}
 	}
 
 	if (command === "moveall") {
-		let userChannel = message.member.voice.channel;
-		let onlineUsers = message.guild.voiceStates.cache;
-		if (!userChannel) {
-			message.channel.send(
-				":x: You are not connected to a voice channel."
-			);
-		} else {
-			//console.log(onlineUsers)
-			if (onlineUsers.size > 0) {
-				//console.log(onlineUsers)
-				onlineUsers.forEach((value, key) => {
-					selectedUser = message.guild.member(key);
-					//console.log(selectedUser);
-					selectedUser.voice.setChannel(userChannel);
-				});
-			} else {
+		if (message.member.roles.some(role => role.name === 'Teacher')) {
+			let userChannel = message.member.voice.channel;
+			let onlineUsers = message.guild.voiceStates.cache;
+			if (!userChannel) {
 				message.channel.send(
-					":x: There is nobody in a voice channel on this server."
+					":x: You are not connected to a voice channel."
 				);
+			} else {
+				//console.log(onlineUsers)
+				if (onlineUsers.size > 0) {
+					//console.log(onlineUsers)
+					onlineUsers.forEach((value, key) => {
+						selectedUser = message.guild.member(key);
+						//console.log(selectedUser);
+						selectedUser.voice.setChannel(userChannel);
+					});
+				} else {
+					message.channel.send(
+						":x: There is nobody in a voice channel on this server."
+					);
+				}
 			}
+		} else {
+			message.reply(":x: You don't have permission to use this command.");
 		}
 	}
 
 	if (command === "setuphand") {
-		message.delete();
-		const exampleEmbed = new MessageEmbed()
-			.setColor("#0099ff")
-			.setTitle("✋ Raise/Lower Hand")
-			.setDescription(
-				"You need to react to this message, but do not spam it."
-			)
-			.addFields(
-				{
-					name: "To raise your hand.",
-					value: "React with this emoji: ✋",
-				},
-				{ name: "To lower your hand.", value: "Remove your reaction." }
-			)
-			.setTimestamp()
-			.setFooter("Setup Timestamp");
+		if (message.member.roles.some(role => role.name === 'Teacher')) {
+			message.delete();
+			const exampleEmbed = new MessageEmbed()
+				.setColor("#0099ff")
+				.setTitle("✋ Raise/Lower Hand")
+				.setDescription(
+					"You need to react to this message, but do not spam it."
+				)
+				.addFields(
+					{
+						name: "To raise your hand.",
+						value: "React with this emoji: ✋",
+					},
+					{ name: "To lower your hand.", value: "Remove your reaction." }
+				)
+				.setTimestamp()
+				.setFooter("Setup Timestamp");
 
-		var embedMSG = message.channel.send(exampleEmbed);
+			var embedMSG = message.channel.send(exampleEmbed);
 
-		(await embedMSG).react("✋");
+			(await embedMSG).react("✋");
+		} else {
+			message.reply(":x: You don't have permission to use this command.");
+		}
 	}
 
 	if (command == "purge") {
-		const amount = args[0]; // Amount of messages which should be deleted
+		if (message.member.roles.some(role => role.name === 'Teacher')) {
+			const amount = args[0]; // Amount of messages which should be deleted
 
-		if (!amount)
-			return message.reply(
-				"You haven't given an amount of messages which should be deleted!"
-			); // Checks if the `amount` parameter is given
-		if (isNaN(amount))
-			return message.reply("The amount parameter isn`t a number!"); // Checks if the `amount` parameter is a number. If not, the command throws an error
+			if (!amount)
+				return message.reply(
+					"You haven't given an amount of messages which should be deleted!"
+				); // Checks if the `amount` parameter is given
+			if (isNaN(amount))
+				return message.reply("The amount parameter isn`t a number!"); // Checks if the `amount` parameter is a number. If not, the command throws an error
 
-		if (amount > 50)
-			return message.reply(
-				"You can`t delete more than 50 messages at once!"
-			); // Checks if the `amount` integer is bigger than 100
-		if (amount < 1)
-			return message.reply("You have to delete at least 1 message!"); // Checks if the `amount` integer is smaller than 1
+			if (amount > 50)
+				return message.reply(
+					"You can`t delete more than 50 messages at once!"
+				); // Checks if the `amount` integer is bigger than 100
+			if (amount < 1)
+				return message.reply("You have to delete at least 1 message!"); // Checks if the `amount` integer is smaller than 1
 
-		await message.channel.messages
-			.fetch({ limit: amount })
-			.then((messages) => {
-				// Fetches the messages
-				message.channel.bulkDelete(
-					messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
-				);
-			});
+			await message.channel.messages
+				.fetch({ limit: amount })
+				.then((messages) => {
+					// Fetches the messages
+					message.channel.bulkDelete(
+						messages // Bulk deletes all messages that have been fetched and are not older than 14 days (due to the Discord API)
+					);
+				});
 
-		console.log(`Deleted ${amount} messages`);
+			console.log(`Deleted ${amount} messages`);
+		} else {
+			message.reply(":x: You don't have permission to use this command.");
+		} 
 	}
 
 	if (command === "servercache") {
-		console.log(message.guild);
-	}
-
-	if (command === "test") {
-		console.log(client.user.username);
+		if (message.member.roles.some(role => role.name === 'Teacher')) {
+			console.log(message.guild);
+		} else {
+			message.reply(":x: You don't have permission to use this command.");
+		}
 	}
 
 	if (command === "absence") {
-		//Command: /absence <time before result>
+		if (message.member.roles.some(role => role.name === 'Teacher')) {
+			//Command: /absence <time before result>
 
-		userTyped = message.author;
+			userTyped = message.author;
 
-		if (!args[0])
-			return message.reply(
-				":x: You need to define a custom time in minutes. !absence <time in minutes>"
+			if (!args[0])
+				return message.reply(
+					":x: You need to define a custom time in minutes. !absence <time in minutes>"
+				);
+			if (isNaN(args[0]))
+				return message.reply(
+					":x: You did not specify a number. !absence <time in minutes>"
+				);
+
+			var timeLeft = args[0] * 60 * 1000;
+
+			message.delete();
+
+			const exampleEmbed = new MessageEmbed()
+				.setColor("#0099ff")
+				.setTitle("✅ Present Check")
+				.setDescription(
+					"If you are present this lesson, react to this message."
+				)
+				.addFields({
+					name: "React to be absence.",
+					value: "React with this emoji: ✅",
+				})
+				.setTimestamp()
+				.setFooter("Absence Date: ");
+
+			var absenceMessage = message.channel.send(exampleEmbed);
+
+			var timeCreated = (await absenceMessage).createdAt;
+
+			(await absenceMessage).react("✅");
+
+			const filter = (reaction, user) => reaction.emoji.name === "✅";
+
+			const collector = (await absenceMessage).createReactionCollector(
+				filter,
+				{ time: timeLeft }
 			);
-		if (isNaN(args[0]))
-			return message.reply(
-				":x: You did not specify a number. !absence <time in minutes>"
-			);
-
-		var timeLeft = args[0] * 60 * 1000;
-
-		message.delete();
-
-		const exampleEmbed = new MessageEmbed()
-			.setColor("#0099ff")
-			.setTitle("✅ Present Check")
-			.setDescription(
-				"If you are present this lesson, react to this message."
-			)
-			.addFields({
-				name: "React to be absence.",
-				value: "React with this emoji: ✅",
-			})
-			.setTimestamp()
-			.setFooter("Absence Date: ");
-
-		var absenceMessage = message.channel.send(exampleEmbed);
-
-		var timeCreated = (await absenceMessage).createdAt;
-
-		(await absenceMessage).react("✅");
-
-		const filter = (reaction, user) => reaction.emoji.name === "✅";
-
-		const collector = (await absenceMessage).createReactionCollector(
-			filter,
-			{ time: 10000 }
-		);
-		//collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
-		collector.on("end", (collected) => {
-			let reaction = collected.first();
-			//console.log(reaction);
-			//console.log("-----------------------");
-			//console.log(reaction.users.cache);
-			var i = 1;
-			var finalList = [];
-			var creatingList = new Promise((resolve, reject) => {
-				reaction.users.cache.forEach((element) => {
-					if (i == 1) {
+			//collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+			collector.on("end", (collected) => {
+				let reaction = collected.first();
+				//console.log(reaction);
+				//console.log("-----------------------");
+				//console.log(reaction.users.cache);
+				var i = 1;
+				var finalList = [];
+				var creatingList = new Promise((resolve, reject) => {
+					reaction.users.cache.forEach((element) => {
+						if (i == 1) {
+							i += 1;
+							return;
+						}
 						i += 1;
-						return;
-					}
-					i += 1;
-					//console.log(element)
-					var serverUsername = message.guild.members.fetch(
-						element.id
-					);
-					serverUsername.then((user) => {
-						//console.log(user.nickname)
-						finalList.push(user.nickname);
+						//console.log(element)
+						var serverUsername = message.guild.members.fetch(
+							element.id
+						);
+						serverUsername.then((user) => {
+							//console.log(user.nickname)
+							finalList.push(user.nickname);
+						});
 					});
+					resolve();
 				});
-				resolve();
+				creatingList.then(() => {
+					//console.log(finalList);
+
+					topLine = "------------------------------------------------------------- **Present Status** -------------------------------------------------------------\n"
+					bottomLine = "\n---------------------------------------------------------------------------------------------------------------------------------------------"
+
+					outputMessage = topLine + "The present status you created at: " + "`" + timeCreated + "` is finish, and the results are here. \n\n **These students were present:**\n" + finalList.map((item, i) => `${i + 1}. ${item}`).join("\r\n") + bottomLine
+
+					userTyped.send(outputMessage);
+				});
 			});
-			creatingList.then(() => {
-				//console.log(finalList);
-
-				topLine = "------------------------------------------------------------- **Present Status** -------------------------------------------------------------\n"
-				bottomLine = "\n---------------------------------------------------------------------------------------------------------------------------------------------"
-
-				outputMessage = topLine + "The present status you created at: " + "`" + timeCreated + "` is finish, and the results are here. \n\n **These students were present:**\n" + finalList.map((item, i) => `${i + 1}. ${item}`).join("\r\n") + bottomLine
-
-				userTyped.send(outputMessage);
-			});
-		});
+		} else {
+			message.reply(":x: You don't have permission to use this command.");
+		}
 	}
 
 	if (command === "help") {
